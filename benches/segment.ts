2d826e1939007cd37335777d1039d86ba11725c4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-04 12:16:20 +0000

    perf: add cases

--- benches/segment.ts
+++ benches/segment.ts
@@ -5,6 +5,7 @@ import { toArrayAsync } from 'iterable-operator'
 import { cedarwood, daachorse, BehaviorForUnmatched } from '..'
 import path from 'path'
 import fs from 'fs/promises'
+import * as Jieba from '@node-rs/jieba'
 
 const patternsFilename = path.join(__dirname, './patterns.txt')
 const samplesFilename = path.join(__dirname, './samples.txt')
@@ -16,9 +17,38 @@ const benchmark = new Benchmark('segment', {
 
 go(async () => {
   const text = await fs.readFile(patternsFilename, 'utf-8')
-  const patterns = text.split('\n').filter(x => !!x)
+  const patterns = Array.from(new Set(
+    text
+      .split('\n')
+      .map(x => x.replace(/\s/g, ''))
+      .filter(x => !!x)
+  ))
 
   const samples = await toArrayAsync(readFileLineByLine(samplesFilename))
+
+  // Jieba的词典只能导入一次, 所以不能在case里导入.
+  const dict = Buffer.from(
+    patterns
+      .map(x => `${x} 0 n`)
+      .join('\n')
+  )
+  Jieba.loadDict(dict)
+
+  benchmark.addCase('@node-rs/jieba (cutForSearch)', () => {
+    return () => {
+      for (const line of samples) {
+        Jieba.cutForSearch(line)
+      }
+    }
+  })
+
+  benchmark.addCase('@node-rs/jieba (cutAll)', () => {
+    return () => {
+      for (const line of samples) {
+        Jieba.cutAll(line)
+      }
+    }
+  })
 
   benchmark.addCase('cedarwood', () => {
     const dict = new cedarwood.ForwardDictionary(patterns)
